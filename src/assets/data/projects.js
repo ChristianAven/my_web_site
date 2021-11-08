625fa2d9279afdb56a267bb2fb96ac346aefb197
Christian Avendaño <chrisavendano0508@gmail.com>
2021-11-07 23:47:56 -0500

    Projects data restructured

---
+++ src/assets/data/projects.js
@@ -0,0 +1,64 @@
+const projects = [
+    {
+        name: "CountryInfo",
+        technology: "React native",
+        date: "29/10/2021",
+        repository: "https://github.com/ChristianAven/CountryInfo"
+    },
+    {
+        name: "RutasApp",
+        technology: "React native",
+        date: "23/07/2021",
+        repository: "https://github.com/ChristianAven/RutasApp"
+    },
+    {
+        name: "React-native-peliculas",
+        technology: "React native",
+        date: "29/06/2021",
+        repository: "https://github.com/ChristianAven/React-native-peliculas"
+    },
+    {
+        name: "Chat-frontend",
+        technology: "React JS",
+        date: "08/02/2021",
+        repository: "https://github.com/ChristianAven/chat-frontend"
+    },
+    {
+        name: "Chat-backend",
+        technology: "Node JS - Socket.IO",
+        date: "08/02/2021",
+        repository: "https://github.com/ChristianAven/Chat-backend"
+    },
+    {
+        name: "Tickets-sockets-frontend",
+        technology: "React JS",
+        date: "24/01/2021",
+        repository: "https://github.com/ChristianAven/tickets-sockets-frontend"
+    },
+    {
+        name: "Tickets-sockets-backend",
+        technology: "Node JS - Socket.IO",
+        date: "24/01/2021",
+        repository: "https://github.com/ChristianAven/tickets-sockets-backend"
+    },
+    {
+        name: "Maps-sockets-frontend",
+        technology: "React JS",
+        date: "26/01/2021",
+        repository: "https://github.com/ChristianAven/Maps-sockets-frontend"
+    },
+    {
+        name: "Maps-sockets-backend",
+        technology: "Node JS - Socket.IO",
+        date: "26/01/2021",
+        repository: "https://github.com/ChristianAven/maps-sockets-backend"
+    },
+    {
+        name: "This web site",
+        technology: "React JS",
+        date: "02/11/2021",
+        repository: "https://github.com/ChristianAven/my_web_site"
+    },
+]
+
+export default projects;
