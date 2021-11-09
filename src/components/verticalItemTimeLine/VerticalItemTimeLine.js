59f66167c46dc4a4f89b84b7f1d7232fadad1fa3
Christian Avendaño <chrisavendano0508@gmail.com>
2021-11-08 19:56:48 -0500

    Time Line restructured

---
+++ src/components/verticalItemTimeLine/VerticalItemTimeLine.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { VerticalTimelineElement } from 'react-vertical-timeline-component'
+
+const VerticalItemTimeLine = ({ date, icon, title, subTitle, textBody }) => {
+    return (
+        <VerticalTimelineElement
+            contentStyle={{ background: 'white', color: 'black', border: '3px solid #0a66c2' }}
+            contentArrowStyle={{ borderRight: '7px solid  #0a66c2' }}
+            date={date}
+            dateClassName={"date_element"}
+            iconStyle={{ color: 'black', border: '3px solid #0a66c2', backgroundColor: 'white' }}
+            icon={icon}
+            iconClassName={"icon_element"}
+            >
+            <h3 className="vertical-timeline-element-title">{title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{subTitle}</h4>
+            <p>
+            {textBody}
+            </p>
+        </VerticalTimelineElement>
+    )
+}
+
+export default VerticalItemTimeLine
--- src/screens/nowScreen/NowScreen.js
+++ src/screens/nowScreen/NowScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import CardHeader from '../../components/cardHeader/CardHeader'
+import VerticalItemTimeLine from '../../components/verticalItemTimeLine/VerticalItemTimeLine';
 import './nowScreen.css'
 
 const NowScreen = () => {
@@ -11,63 +12,39 @@ const NowScreen = () => {
 
             <div>
                 <VerticalTimeline lineColor='#0a66c2' >
-                    <VerticalTimelineElement
-                        contentStyle={{ background: 'white', color: 'black', border: '3px solid rgb(33, 150, 243)' }}
-                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                        date="Present - 2021/December"
-                        dateClassName={"date_element"}
-                        iconStyle={{ color: 'black', border: '3px solid #0a66c2', backgroundColor: 'white' }}
-                        icon={<i className="fab fa-react"></i>}
-                        iconClassName={"icon_element"}
-                        >
-                        <h3 className="vertical-timeline-element-title">React JS advanced</h3>
-                        <h4 className="vertical-timeline-element-subtitle">With certificate</h4>
-                        <p>
-                        Storybook, Github Actions, publish NPM, TypeScript, component patterns, PWA, Formik, dynamic forms, and more
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        contentStyle={{ background: 'white', color: 'black', border: '3px solid rgb(33, 150, 243)' }}
-                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                        date="2022/January - 2022/May"
-                        icon={<i className="fab fa-docker"></i>}
-                        iconStyle={{ color: 'black', border: '3px solid #0a66c2', backgroundColor: 'white' }}
-                        iconClassName={"icon_element"}
-                        >
-                        <h3 className="vertical-timeline-element-title">Docker</h3>
-                        <h4 className="vertical-timeline-element-subtitle">With certificate</h4>
-                        <p>
-                        Learn how to use Docker from the basics to Docker Compose, Docker Swarm, and Kubernetes
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        contentStyle={{ background: 'white', color: 'black', border: '3px solid rgb(33, 150, 243)' }}
-                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                        date="2022/June - 2022/December"
-                        icon={<i className="fab fa-aws"></i>}
-                        iconStyle={{ color: 'black', border: '3px solid #0a66c2', backgroundColor: 'white' }}
-                        iconClassName={"icon_element"}
-                    >
-                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                        <p>
-                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        contentStyle={{ background: 'white', color: 'black', border: '3px solid rgb(33, 150, 243)' }}
-                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                        date="2023/January - 2023/June"
-                        icon={<i className="fab fa-java"></i>}
-                        iconStyle={{ color: 'black', border: '3px solid #0a66c2', backgroundColor: 'white' }}
-                        iconClassName={"icon_element"}
-                        >
-                        <h3 className="vertical-timeline-element-title">Master in JAVA</h3>
-                        <h4 className="vertical-timeline-element-subtitle">With certificate</h4>
-                        <p>
-                        Java EE 9, CDI, JPA, EJB, Web and more
-                        </p>
-                    </VerticalTimelineElement>
+                    
+                    <VerticalItemTimeLine 
+                        date={"Present - 2021/December"} 
+                        icon={<i className="fab fa-react"></i>} 
+                        subTitle="With certificate"
+                        title="React JS advanced"
+                        textBody="Storybook, Github Actions, publish NPM, TypeScript, component patterns, PWA, Formik, dynamic forms, and more"
+                    />
+                    
+                    <VerticalItemTimeLine 
+                        date={"2022/January - 2022/May"} 
+                        icon={<i className="fab fa-docker"></i>} 
+                        subTitle="With certificate"
+                        title="Docker"
+                        textBody="Learn how to use Docker from the basics to Docker Compose, Docker Swarm, and Kubernetes"
+                    />
+                    
+                    <VerticalItemTimeLine 
+                        date={"2022/June - 2022/December"} 
+                        icon={<i className="fab fa-aws"></i>} 
+                        subTitle="With certificate"
+                        title="AWS"
+                        textBody="EC2, Light Sail, Elastic Beanstalk, EKS, AWS Lambda, DMS, SMS, Snowball, Amazon Glacier, Amazon Elastic Block Store (EBS) and more"
+                    />
+                    
+                    <VerticalItemTimeLine 
+                        date={"2023/January - 2023/June"} 
+                        icon={<i className="fab fa-java"></i>} 
+                        subTitle="With certificate"
+                        title="Master in JAVA*"
+                        textBody="Java EE 9, CDI, JPA, EJB, Web and more"
+                    />
+
                 </VerticalTimeline>
             </div>
 
