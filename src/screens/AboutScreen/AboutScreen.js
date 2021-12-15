2d22832f62406366c84dfa15aecfb5e799073148
Christian David Avendaño Mora <chrisavendano0508@gmail.com>
2021-12-15 14:42:11 -0500

    about screen redesigned

--- src/screens/AboutScreen/AboutScreen.js
+++ src/screens/AboutScreen/AboutScreen.js
@@ -3,6 +3,7 @@ import CardHeader from '../../components/cardHeader/CardHeader'
 import 'animate.css';
 import './aboutScreen.css'
 import { DarkModeContext } from '../../context/DarkModeContext';
+import myImg from '../../assets/my_photo.jpg'
 
 const HomeScreen = () => {
 
@@ -13,28 +14,45 @@ const HomeScreen = () => {
             
             <CardHeader icon='fas fa-user-tie' title='About me'/>
 
-            <div className='card_body'>
-                <div>
-                    <div className='header_cardBody'>
-                        <div>
-                            <h4 translate="no"><i className="fas fa-signature header_cardBody_icon" style={ darkMode ? {color: "#a7adb4"} : {}}></i> Full name: Christian David Avendaño Mora</h4>
+            <div className='card_body_my'>
+                <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <div className='card_general_information' style={darkMode ? {boxShadow: "none"} : {border: "1px solid #c4cad1"}}>
+                        <div className='bg_card'/>
+                        <div className='my_card_body_container'>
+                            <div className='my_card_body'>
+                                <div>
+                                    <img className='my_photo' src={myImg}/>
+                                </div>
+                                <div>
+                                    <span style={{color: "black"}}><strong>Christian David</strong> 20</span>
+                                </div>
+                                <div>
+                                    <span style={{color: "black", fontSize: "13px"}}>Spanish - Colombian</span>
+                                </div>
+                                <hr color='#d1d1d1' style={{ width: '100%', height: "0px", marginTop: "35px"}}/>
+                            </div>
                         </div>
-                        <div>
-                            <h4>&nbsp;&nbsp;&nbsp;&nbsp; <i className="fas fa-birthday-cake header_cardBody_icon" style={ darkMode ? {color: "#a7adb4"} : {}}></i> Date of birth: 2001-05-8</h4>
-                        </div>
-                        <div>
-                            <h4>&nbsp;&nbsp;&nbsp;&nbsp; <i className="far fa-flag header_cardBody_icon" style={ darkMode ? {color: "#a7adb4"} : {}}></i> Nationality: Spanish - Colombian</h4>
+                        <div className='my_card_footer'>
+                            <div className='my_card_footer_item'>
+                                <a className='link_linkedin' href='https://www.linkedin.com/in/chris-aven/' target='_blank' rel="noreferrer"  ><i className="fab fa-linkedin"></i></a>
+                                <span><strong>Linkedin</strong></span>    
+                            </div>
+                            <div className='my_card_footer_item'>
+                            <a className='link_twiter' href='https://twitter.com/AVENNM_' target='_blank' rel="noreferrer" ><i className="fab fa-twitter"></i></a>
+                                <span><strong>Twitter</strong></span>
+                            </div>
+                            <div className='my_card_footer_item'>
+                            <a className='link_github' href='https://github.com/ChristianAven' target='_blank' rel="noreferrer" ><i className="fab fa-github"></i></a>
+                                <span><strong>Github</strong></span>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div style={{marginLeft: "60px", marginRight: "60px"}}>
                     <p><strong>»</strong> Hello! I am a systems engineer and I focus on frontend development with good knowledge in backend development.</p>
                     <p><strong>»</strong> I have the ability to propose new solutions and develop customer needs in order to obtain better results.</p>
                     <p><strong>»</strong> Technical and theoretical professional knowledge in different technologies (with their respective certificates), high commitment to projects, development and final delivery. analytical skills, leadership, teamwork and constant learning.</p>
                     <p><strong>»</strong> I would very much like that in my next project to be surrounded by a team which can learn too much from them and do what I am passionate about, programming and learning new things every day!</p>
-                </div>
-                <div className='contact_linkedin' >
-                    <span>More information in:</span>
-                    <a className='link_linkedin' style={ darkMode ? {color: "#fff"} : {}} href='https://www.linkedin.com/in/chris-aven/' target='_blank' rel="noreferrer"  ><i className="fab fa-linkedin"></i></a>
-                    <a className='link_github' style={ darkMode ? {color: "#fff"} : {}} href='https://github.com/ChristianAven' target='_blank' rel="noreferrer" ><i className="fab fa-github"></i></a>
                 </div>
             
             </div>
